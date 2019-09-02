c02bccafc1c7b579a66ed172662eb36778b32a61
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2019-09-02 18:43:05 +0430

    :recycle: use markdown for documentation

--- src/components/2-organisms/Documentations/dropdown/dropdown.vue
+++ src/components/2-organisms/Documentations/dropdown/dropdown.vue
@@ -1,7 +1,37 @@
 /* eslint-disable */
 <template>
   <div class="dropdown">
-    <h1>
+    <markdown-parser>
+      # Dropdown
+
+      ## Installation
+
+      <pre><prism language="javascript">
+      npm i @ghostax/dropdown
+      // or
+      yarn add @ghostax/dropdown
+      </prism></pre>
+
+      ## Demo
+
+      <dropdown-demo />
+
+      ## Usage
+
+      <pre><prism language="javascript">
+      import { Dropdownue, DropdownueItem } from "@ghostax/dropdown";
+
+      export default {
+        ...
+        components: {
+          Dropdownue,
+          DropdownueItem
+        }
+        ...
+      };
+      </prism></pre>
+    </markdown-parser>
+    <!-- <h1>
       Dropdown
     </h1>
     <h2>
@@ -23,6 +53,13 @@
     <h2>
       Usage
     </h2>
+    <prism language="html">
+      {{`
+      <div>
+        <strong>foo</strong>
+      </div>
+      `}}
+    </prism>
     <pre><prism language="javascript">
       import { Dropdownue, DropdownueItem } from "@ghostax/dropdown";
 
@@ -34,20 +71,21 @@
         }
         ...
       };
-    </prism></pre>
+    </prism></pre> -->
   </div>
 </template>
 
 <script>
 import Prism from 'vue-prism-component';
-import 'prismjs/components/prism-bash';
+import MarkdownParser from '~organisms/MarkdownParser/MarkdownParser';
 import DropdownDemo from './DropdownDemo';
 
 export default {
   name: 'Dropdown',
   components: {
-    Prism,
-    DropdownDemo
+    MarkdownParser,
+    DropdownDemo,
+    Prism
   },
   props: {},
   data() {
