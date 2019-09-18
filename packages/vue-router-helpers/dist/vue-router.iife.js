a1c772c120ea183cc100b836d5c6e2f22019ddbf
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2019-09-18 12:11:21 +0430

    :see_no_evil: Ignore all dist folders

--- packages/vue-router-helpers/dist/vue-router.iife.js
+++
@@ -1,2 +0,0 @@
-var VueRouter=function(r){"use strict";var n=require("obj-traverse/lib/obj-traverse");return r.findRawRoute=function(r,u){var e,t=null==r?void 0:null===(e=r.options)||void 0===e?void 0:e.routes;if(!t)return null;var i=null;return t.some(function(r){return i=n.findFirst(r,"children",u)}),i||null},r}({});
-//# sourceMappingURL=vue-router.iife.js.map
