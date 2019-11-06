e8455870024115cb8a649555f2e64c07b6549910
Mehdi Hoseini <mehdi.hoseinipajooh@gmail.com>
2019-11-06 23:59:57 +0330

    :sparkles: Add next/prev route helpers

--- packages/vue-router-helpers/bili.config.js
+++ packages/vue-router-helpers/bili.config.js
@@ -1,36 +1,36 @@
 import pkg from './package.json';
 
 export default {
-  input: './src/vue-router.js',
-  output: {
-    moduleName: 'VueRouter',
-    minify: true,
-    format: [
-      'cjs',
-      'umd',
-      'esm',
-      'iife'
-    ],
-    dir: 'dist'
-  },
-  externals: [
-    ...Object.keys(pkg.peerDependencies || {})
-  ],
-  plugins: {
-    commonjs: true,
-    'node-resolve': {
-      browser: true,
-      preferBuiltins: false
+    input: './src/vue-router.js',
+    output: {
+        moduleName: 'VueRouter',
+        minify: true,
+        format: [
+            'cjs',
+            'umd',
+            'esm',
+            'iife'
+        ],
+        dir: 'dist'
     },
-    strip: (() => {
-      if (process.env.NODE_ENV !== 'development') {
-        return {
-          debugger: true,
-          functions: ['console.*', 'assert.*', 'debug', 'alert'],
-          sourceMap: true
-        };
-      }
-      return false;
-    })()
-  }
+    externals: [
+        ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: {
+        commonjs: true,
+        'node-resolve': {
+            browser: true,
+            preferBuiltins: false
+        },
+        strip: (() => {
+            if (process.env.NODE_ENV !== 'development') {
+                return {
+                    debugger: true,
+                    functions: ['console.*', 'assert.*', 'debug', 'alert'],
+                    sourceMap: true
+                };
+            }
+            return false;
+        })()
+    }
 };
--- packages/vue-router-helpers/src/vue-router.js
+++ packages/vue-router-helpers/src/vue-router.js
@@ -1,18 +1,67 @@
 const objTraverser = require('obj-traverse/lib/obj-traverse');
 
 const findRawRoute = ($router, query) => {
-  const routes = $router?.options?.routes;
-  if (!routes) return null;
+    const allRawRoutes = $router?.options?.routes;
+    if (!allRawRoutes) return null;
 
-  let foundedRoute = null;
-  routes.some((route) => {
-    foundedRoute = objTraverser.findFirst(route, 'children', query);
-    return foundedRoute;
-  });
-  return foundedRoute || null;
+    let foundedRoute = null;
+    allRawRoutes.some((route) => {
+        foundedRoute = objTraverser.findFirst(route, 'children', query);
+        return foundedRoute;
+    });
+    return foundedRoute || null;
 };
 
 
+const _flatRouteChildren = (parentRoute, flattendChildren = []) => {
+    if (parentRoute.children && parentRoute.children.length) {
+        parentRoute.children.forEach((childRoute) => {
+            if (childRoute.children && childRoute.children.length) {
+                if (!childRoute.redirect) {
+                    flattendChildren.push(childRoute);
+                }
+                return _flatRouteChildren(childRoute, flattendChildren);
+            }
+            if (!childRoute.redirect) {
+                flattendChildren.push(childRoute);
+            }
+        });
+    }
+    return flattendChildren;
+};
+
+const findNextAbsoluteRoute = ($router, $route) => {
+    const query = { meta: { isRootNavigation: true } };
+    const rawRootParent = findRawRoute($router, query);
+    const flattenRawRoutes = _flatRouteChildren(rawRootParent);
+    const currentRouteIndex = flattenRawRoutes.findIndex((route) => { return route.name === $route.name; });
+    return flattenRawRoutes?.[currentRouteIndex + 1];
+};
+const findPrevAbsoluteRoute = ($router, $route) => {
+    const query = { meta: { isRootNavigation: true } };
+    const rawRootParent = findRawRoute($router, query);
+    const flattenRawRoutes = _flatRouteChildren(rawRootParent);
+    const currentRouteIndex = flattenRawRoutes.findIndex((route) => { return route.name === $route.name; });
+    return flattenRawRoutes?.[currentRouteIndex - 1];
+};
+
+const findNextRelativeRoute = ($router, $route) => {
+    const parentName = $route.matched[$route.matched.length - 2].name;
+    const rawRootParent = findRawRoute($router, { name: parentName });
+    const currentRouteIndex = rawRootParent.children.findIndex((route) => { return route.name === $route.name; });
+    return rawRootParent?.children?.[currentRouteIndex + 1];
+};
+const findPrevRelativeRoute = ($router, $route) => {
+    const parentName = $route.matched[$route.matched.length - 2].name;
+    const rawRootParent = findRawRoute($router, { name: parentName });
+    const currentRouteIndex = rawRootParent.children.findIndex((route) => { return route.name === $route.name; });
+    return rawRootParent?.children?.[currentRouteIndex - 1];
+};
+
 export {
-  findRawRoute
+    findRawRoute,
+    findNextAbsoluteRoute,
+    findPrevAbsoluteRoute,
+    findNextRelativeRoute,
+    findPrevRelativeRoute
 };
